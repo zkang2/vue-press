9ba3ac5aeda83b77ac551b0248a2e1772bd48090
zkk <2862343054@qq.com>
2023-08-12 17:59:31 +0800

    更新依赖 添加代码块复制

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,71 +1,76 @@
-import { defineUserConfig, defaultTheme } from "vuepress";
-import { searchPlugin } from '@vuepress/plugin-search'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-export default({
-  lang: "zh-CN",
-  title: "记录小屋!",
-  base: "/", // 打包路径
-  // open:true,  //在开发服务器启动后打开浏览器
-  plugins: [
-    docsearchPlugin({
-      appId: 'QMR3KMGTUV',
-      apiKey:'c90386e8287ae66200330368100663d5',
-      indexName: 'kangkang',
-      placeholder: '搜索文档',
-      translations: {
-        button: {
-          buttonText: '搜索文档',
-        },
-      },
-    }),
-  ],
-  head: [['link', { rel: 'icon', href: '/images/logo.jpg' }]],
-  description: "这是我的第一个 VuePress 站点",
-  theme: defaultTheme({
-    lastUpdatedText:'更新时间',
-    contributorsText:'贡献者',
-    editLink:false,
-    // 仓库地址
-    repo: 'https://gitee.com/zkang2/vue-press.git',
-    repoLabel:'🧐gitee',
-    logo: "/images/logo.jpg",
-    // 404 页面的提示信息
-    notFound:['糟糕！这个页面找不到啦！！！'],
-    backToHome:'返回首页',
-    // 导航栏配置
-    navbar: [
-      {
-        text: "‍🔧工具库",
-        children:[
-            {text:'nvm', link: '/views/nvm/'},
-            {text:'vconsole', link: '/views/vconsole/'}
-        ]
-      },
-      {
-        text: "☠️推荐依赖",
-        children:[
-          {text:'发布npm包', link: '/views/npm/'},
-          {text:'快速删除 node_modules', link: '/views/head/'}
-        ]
-      }
+import {defineUserConfig, defaultTheme} from "vuepress";
+// 搜索
+import {docsearchPlugin} from '@vuepress/plugin-docsearch'
+// 快捷复制代码块
+import {copyCodePlugin} from "vuepress-plugin-copy-code2";
+export default defineUserConfig({
+    lang: "zh-CN",
+    title: "记录小屋!",
+    base: "/", // 打包路径
+    // open:true,  //在开发服务器启动后打开浏览器
+    plugins: [
+        copyCodePlugin({
+            showInMobile:true,
+        }),
+        docsearchPlugin({
+            appId: 'QMR3KMGTUV',
+            apiKey: 'c90386e8287ae66200330368100663d5',
+            indexName: 'kangkang',
+            placeholder: '搜索文档',
+            translations: {
+                button: {
+                    buttonText: '搜索文档',
+                },
+            },
+        }),
     ],
-    sidebar: [
-      {
-        text: "首页",
-        link: "/views/list/",
-      },
-      {
-        text: "uniapp",
-        link: "/views/uniapp/",
-      },
-      {
-        text: "ES6",
-        link: "/views/es6/",
-      },
-      {
-        text: "Vue",
-        link: "/views/vue/",
-      },
-    ]
-  }),
-});
+    head: [['link', {rel: 'icon', href: '/images/logo.jpg'}]],
+    description: "这是我的第一个 VuePress 站点",
+    theme: defaultTheme({
+        lastUpdatedText: '更新时间',
+        contributorsText: '贡献者',
+        editLink: false,
+        // 仓库地址
+        repo: 'https://gitee.com/zkang2/vue-press.git',
+        repoLabel: '🧐gitee',
+        logo: "/images/logo.jpg",
+        // 404 页面的提示信息
+        notFound: ['糟糕！这个页面找不到啦！！！'],
+        backToHome: '返回首页',
+        // 导航栏配置
+        navbar: [
+            {
+                text: "‍🔧工具库",
+                children: [
+                    {text: 'nvm', link: '/views/nvm/'},
+                    {text: 'vconsole', link: '/views/vconsole/'}
+                ]
+            },
+            {
+                text: "☠️推荐依赖",
+                children: [
+                    {text: '发布npm包', link: '/views/npm/'},
+                    {text: '快速删除 node_modules', link: '/views/head/'}
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                text: "首页",
+                link: "/views/list/",
+            },
+            {
+                text: "uniapp",
+                link: "/views/uniapp/",
+            },
+            {
+                text: "ES6",
+                link: "/views/es6/",
+            },
+            {
+                text: "Vue",
+                link: "/views/vue/",
+            },
+        ]
+    }),
+})
