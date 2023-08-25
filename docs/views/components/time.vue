f31f8327da9aee93d47f984fc69050b5e9ca05fc
kk <zhangkangkang@fbkjgf.wecom.work>
2023-08-25 16:27:23 +0800

    添加 沉浸阅读切换

--- docs/views/components/time.vue
+++ docs/views/components/time.vue
@@ -2,17 +2,24 @@
   <div class="content">
     <el-button type="primary" :icon="Timer" plain>{{ readingTimeLocale.time }}</el-button>
     <el-button type="primary" :icon="ChatLineRound" plain>{{ readingTimeLocale.words }}</el-button>
+    <el-switch
+        v-model="switchValue"
+        inline-prompt
+        size="large"
+        style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+        active-text="沉浸阅读"
+        inactive-text="粒子背景"
+    />
     <el-divider>
       <el-divider>{{ state.currentDateTime }}</el-divider>
     </el-divider>
-    <Particles id="tsparticles" class="login-particles" :particlesInit="particlesInit"
-               :particlesLoaded="particlesLoaded" :options="options"/>
+    <Particles v-if="switchValue" id="tsparticles" class="login-particles" :particlesInit="particlesInit" :particlesLoaded="particlesLoaded" :options="options"/>
   </div>
 </template>
 <script setup>
-import {reactive, onMounted} from 'vue';
+import {reactive, onMounted,ref } from 'vue';
 import {useReadingTimeLocale} from "vuepress-plugin-reading-time2/client";
-
+const switchValue = ref(true)
 const readingTimeLocale = useReadingTimeLocale();
 import {ChatLineRound, Timer} from '@element-plus/icons-vue';
 
@@ -38,5 +45,10 @@ const particlesLoaded = async (container) => {
 <style scoped lang="scss">
 .content {
   padding: 20px 0;
+}
+.el-switch{
+  position: relative;
+  z-index: 1;
+  margin: 0 10px;
 }
 </style>
