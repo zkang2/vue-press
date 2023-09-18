0d24de512481940fa9221a200f4d466ba91d15cb
kk <zhangkangkang@fbkjgf.wecom.work>
2023-09-18 18:20:15 +0800

    替换二维码插件

--- docs/.vuepress/components/Qrcode.vue
+++ docs/.vuepress/components/Qrcode.vue
@@ -1,31 +1,44 @@
-<script setup>
-import { Download } from '@element-plus/icons-vue'
-import {ref, watch, nextTick} from 'vue'
-const input = ref('https://www.kangkang.cloud/')
-import {Vue3NextQrcode} from 'vue3-next-qrcode'
-const is = ref(true)
-watch(input, async (newVal, oldVal) => {
-  is.value = false
-  await nextTick()
-  is.value = true
-})
-
-const fn = (url)=>{
-  console.log(url)
-}
-const downloadImage=()=> {
-  const imgElements = document.getElementsByTagName('img');
-  const link = document.createElement('a');
-  link.href = imgElements[1].currentSrc;
-  link.download = 'image.jpg';
-  link.click();
-}
-</script>
-
 <template>
   <div style="display: flex">
     <el-input v-model="input" clearable placeholder="输入内容更新二维码"/>
     <el-button type="primary" @click="downloadImage">下载图片<el-icon class="el-icon--right"><Download /></el-icon></el-button>
   </div>
-  <Vue3NextQrcode v-if="is" :text="input" @onSuccess="fn"  :dotScale="0.7" gifBackgroundURL="/images/bg.gif" logoImage="/images/logo.jpg" :logoScale="0.2" :logoMargin="3" />
+  <el-image style="width: 150px; height: 150px" :src="imgSrc" fit="cover"/>
 </template>
+
+<script setup>
+import {Download} from '@element-plus/icons-vue'
+import QRCode from 'qrcode'
+import {onMounted, ref, watch} from "vue";
+const input = ref('https://www.kangkang.cloud/')
+const imgSrc = ref(null)
+watch(input, async (newVal, oldVal) => {
+  init()
+})
+var opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/png',
+  quality: 0.8,
+  margin: 1,
+  color: {
+    dark: "#000",
+    light: "#fff"
+  }
+}
+onMounted(() => {
+  init()
+})
+const downloadImage=()=> {
+  const link = document.createElement('a');
+  link.href = imgSrc.value;
+  link.download = 'myimage.png';
+  link.click();
+}
+const init = () => {
+  QRCode.toDataURL(input.value, opts).then(url => {
+    imgSrc.value = url
+  }).catch(err => {
+    console.error(err)
+  })
+}
+</script>
